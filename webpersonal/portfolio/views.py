2e74370f3d80ae0ee4720013bfeae4ebba642e40
Irbingou <irving.aav92@gmail.com>
2021-02-18 21:14:48 -0600

    Iteración de objetos en template

--- webpersonal/portfolio/views.py
+++ webpersonal/portfolio/views.py
@@ -5,7 +5,8 @@ from .models import Project
 
 
 def portfolio(request):
-    # se define una variable para recuperar la lista de proyectos guardada en el modelo Projects
+    # se define una variable para recuperar la
+    # lista de proyectos guardada en el modelo Projects
     project = Project.objects.all()
     # para pasar datos a un template, se utliza un parametro 
     # conocido como diccionario de contexto y se asigna un nombre
